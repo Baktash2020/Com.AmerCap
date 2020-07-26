a3f1ab5f9782aa31ff6e827fdae5a074a586a561
Baktash <wnillab@gmail.com>
2020-07-26 17:45:58 -0400

    Updated on 7/26/2020

--- src/test/java/TestNg/Com/AmerCap/ConventionalLoans.java
+++ src/test/java/TestNg/Com/AmerCap/ConventionalLoans.java
@@ -10,7 +10,7 @@ public class ConventionalLoans {
 	public static void main(String[] args) throws InterruptedException {
 		
 
-		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aqdas\\Desktop\\GoogleChrome\\chromedriver.exe");
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aqdas\\Desktop\\Chromed\\chromedriver.exe");
 			
 			WebDriver driver = new ChromeDriver();
 			
--- src/test/java/TestNg/Com/AmerCap/LoanProgramsDropDowns.java
+++ src/test/java/TestNg/Com/AmerCap/LoanProgramsDropDowns.java
@@ -13,7 +13,7 @@ public class LoanProgramsDropDowns {
 	public static void main(String[] args) throws InterruptedException {
 
 
-       System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aqdas\\Desktop\\GoogleChrome\\chromedriver.exe");
+       System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aqdas\\Desktop\\Chromed\\chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		
